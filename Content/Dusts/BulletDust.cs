e56fe2a35b3fa842a3249248c2ee342d2bfba4fe
elmage78 <99903003+elmage78@users.noreply.github.com>
2023-10-05 08:45:04 +0000

    Added branches for version security
    Added branches for version security & modified BulletDust to test Predraw & Postdraw (kinda like shaders functions)

--- Content/Dusts/BulletDust.cs
+++ Content/Dusts/BulletDust.cs
@@ -1,4 +1,4 @@
-﻿using Terraria;
+using Terraria;
 using Terraria.ModLoader;
 
 namespace zapitanor.Content.Dusts
@@ -10,9 +10,20 @@ namespace zapitanor.Content.Dusts
             dust.velocity *= 0.6f; // Multiply the dust's start velocity by 0.6, slowing it down
             dust.noGravity = false; // Makes the dust have gravity.
             dust.noLight = true; // Makes the dust emit no light.
-            dust.scale *= 1.5f; // Multiplies the dust's initial scale by 1.5.
+            dust.scale *= 0.5f; // Multiplies the dust's initial scale by 0.5
             dust.alpha *= 1;
         }
+        public override bool PreDraw(Microsoft.Xna.Framework.Graphics.SpriteBatch spriteBatch, Microsoft.Xna.Framework.Color lightColor)
+        {
+            Vector2 zero = Vector2.Zero;
+            SpriteEffects effects1 = SpriteEffects.None;
+            if (dust.spriteDirection == -1)
+            {
+                zero.X = (float)Main.dustTexture[dust.type].Width; 
+				effects1 = SpriteEffects.FlipHorizontally;
+            }
+            Main.spriteBatch.Draw(Main.dustTexture[dust.type], new Vector2(dust.position.X - Main.screenPosition.X +   (float)  (dust.width / 2), dust.position.Y - Main.screenPosition.Y + (float)(dust.height / 2) + dust.gfxOffY), new Microsoft.Xna.Framework.Rectangle?(new Microsoft.Xna.Framework.Rectangle(0, 0, Main.dustTexture[dust.type].Width, Main.dustTexture[dust.type].Height)), dust.GetAlpha(lightColor), dust.rotation, zero, dust.scale, effects1, 0.0f);
+		}
 
         public override bool Update(Dust dust)
         { // Calls every frame the dust is active
